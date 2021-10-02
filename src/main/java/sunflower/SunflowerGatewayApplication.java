461b72d9103423aef638061e3c5bbf96fef85c76
Izumi <1042631369@qq.com>
2021-10-02 10:54:12 +0800

    [Izumi] init: add SunflowerGatewayApplication

---
+++ src/main/java/sunflower/SunflowerGatewayApplication.java
@@ -0,0 +1,11 @@
+package sunflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunflowerGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SunflowerGatewayApplication.class, args);
+    }
+}
