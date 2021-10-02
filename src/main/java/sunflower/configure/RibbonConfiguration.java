ef88dfd78ac669ad6395194f58d854d317d1949d
Izumi <1042631369@qq.com>
2021-10-02 11:38:52 +0800

    [Izumi] feat: ribbon configuration

---
+++ src/main/java/sunflower/configure/RibbonConfiguration.java
@@ -0,0 +1,14 @@
+package sunflower.configure;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RibbonConfiguration {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }}
