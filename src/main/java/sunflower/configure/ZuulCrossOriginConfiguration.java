e02b4f402d457f513a383aa96d2d61d167affb29
Izumi <1042631369@qq.com>
2021-10-11 11:15:13 +0800

    add allowedheaders

--- src/main/java/sunflower/configure/ZuulCrossOriginConfiguration.java
+++ src/main/java/sunflower/configure/ZuulCrossOriginConfiguration.java
@@ -13,6 +13,11 @@ public class ZuulCrossOriginConfiguration {
         corsConfiguration.setMaxAge(3600L); // 表明在3600秒内，不需要再发送预检验请求
         corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 允许http://localhost:7777域名使用
         corsConfiguration.addAllowedOrigin("http://sunflower.linko.top"); // 允许http://localhost:7777域名使用
+        corsConfiguration.addAllowedOrigin("http://sunflower.linko.top:80"); // 允许http://localhost:7777域名使用
+        corsConfiguration.addAllowedOrigin("http://www.linko.top"); // 允许http://localhost:8080域名使用
+        corsConfiguration.addAllowedOrigin("http://www.linko.top:80"); // 允许http://localhost:8080域名使用
+        corsConfiguration.addAllowedOrigin("http://8.140.110.215"); // 允许http://localhost:8080域名使用
+        corsConfiguration.addAllowedOrigin("http://8.140.110.215:80"); // 允许http://localhost:8080域名使用
         corsConfiguration.addAllowedHeader("*"); // 允许任何头
         corsConfiguration.addAllowedMethod("*"); // 允许任何方法（post、get等）
         corsConfiguration.setAllowCredentials(true);
