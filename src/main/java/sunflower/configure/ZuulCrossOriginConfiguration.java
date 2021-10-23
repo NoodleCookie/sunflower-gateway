aa7d8708f788e0cb5fb80b98766cd45091c0fdf4
Izumi <1042631369@qq.com>
2021-10-23 13:42:11 +0800

    允许所有域名跨域访问

--- src/main/java/sunflower/configure/ZuulCrossOriginConfiguration.java
+++ src/main/java/sunflower/configure/ZuulCrossOriginConfiguration.java
@@ -11,13 +11,15 @@ public class ZuulCrossOriginConfiguration {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setMaxAge(3600L); // 表明在3600秒内，不需要再发送预检验请求
-        corsConfiguration.addAllowedOrigin("http://localhost:8081"); // 允许http://localhost:7777域名使用
-        corsConfiguration.addAllowedOrigin("http://sunflower.linko.top"); // 允许http://localhost:7777域名使用
-        corsConfiguration.addAllowedOrigin("http://sunflower.linko.top:80"); // 允许http://localhost:7777域名使用
-        corsConfiguration.addAllowedOrigin("http://www.linko.top"); // 允许http://localhost:8080域名使用
-        corsConfiguration.addAllowedOrigin("http://www.linko.top:80"); // 允许http://localhost:8080域名使用
-        corsConfiguration.addAllowedOrigin("http://8.140.110.215"); // 允许http://localhost:8080域名使用
-        corsConfiguration.addAllowedOrigin("http://8.140.110.215:80"); // 允许http://localhost:8080域名使用
+//        corsConfiguration.addAllowedOrigin("http://localhost:8081"); // 允许http://localhost:7777域名使用
+//        corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 允许http://localhost:7777域名使用
+//        corsConfiguration.addAllowedOrigin("http://sunflower.linko.top"); // 允许http://localhost:7777域名使用
+//        corsConfiguration.addAllowedOrigin("http://sunflower.linko.top:80"); // 允许http://localhost:7777域名使用
+//        corsConfiguration.addAllowedOrigin("http://www.linko.top"); // 允许http://localhost:8080域名使用
+//        corsConfiguration.addAllowedOrigin("http://www.linko.top:80"); // 允许http://localhost:8080域名使用
+//        corsConfiguration.addAllowedOrigin("http://8.140.110.215"); // 允许http://localhost:8080域名使用
+//        corsConfiguration.addAllowedOrigin("http://8.140.110.215:80"); // 允许http://localhost:8080域名使用
+        corsConfiguration.addAllowedOrigin("*"); // 允许http://localhost:8080域名使用
         corsConfiguration.addAllowedHeader("*"); // 允许任何头
         corsConfiguration.addAllowedMethod("*"); // 允许任何方法（post、get等）
         corsConfiguration.setAllowCredentials(true);
